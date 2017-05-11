28cc1c9b3d3079ba82dd0409ca5c0497232613cd
Aleksandr Fedorov <Aleksandr_Fedorov1@epam.com>
2017-05-11 15:50:54 +0300

    solution task

--- src/memoize.js
+++ src/memoize.js
@@ -1,5 +1,22 @@
 export { memoize };
 
 function memoize(func) {
-	// TODO
+    var cash = {};
+    return function (...arg) {
+        var key = identifyKey(arg), result;
+        if (key in cash) {
+            result = cash[key];
+        } else {
+            result = cash[key] = func(...arg);
+        }
+        return result;
+    }
 }
+
+var identifyKey = function (arg) {
+    var key = "";
+    arg.forEach(function (elem) {
+                            key += "[" + Object.prototype.toString.call(elem).slice(8, -1) + " - " + elem + "], ";
+                        });
+    return key;
+};
